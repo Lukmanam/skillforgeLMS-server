9f554438528a6ac4fb925a15d4e75f0a650516ed
Lukmanam <lukkuptrklm2000@gmail.com>
2024-02-14 09:40:04 +0530

    changes in socket io after hosting

--- socketIo.js
+++ socketIo.js
@@ -3,7 +3,7 @@ import { Server } from "socket.io";
 function socketConnection(server) {
     const io = new Server(server, {
         cors: {
-            origin: "http://localhost:5173",
+            origin: "https://skillforge-lms-client.vercel.app",
             methods: ["GET", "POST"],
         },
     });
