e6b0df050257de2c1eb26b3842e762d6c68ac517
Lukmanam <lukkuptrklm2000@gmail.com>
2024-04-13 23:50:34 +0530

    code cleanup

--- controllers/instructorsController.js
+++ controllers/instructorsController.js
@@ -32,7 +32,6 @@ export const registerInstructor = async (req, res) => {
             })
             const instructorData = await instructor.save();
             otpId = await sendMailOtp(instructor.name, instructor.email, instructor._id)
-            console.log(instructorData, "this is instructor data");
             return res
                 .status(201)
                 .json({ message: `otp Has been send to ${email}`, instructor: instructorData, otpId: otpId })
@@ -50,22 +49,16 @@ export const registerInstructor = async (req, res) => {
 export const instructorEmailOtpVerification = async (req, res) => {
     try {
         const { otp, instructorId } = req.body;
-        console.log("instructorId =", instructorId);
-        console.log("OTP=", otp);
         const otpData = await Otp.find({ studentId: instructorId });
-        console.log(otpData);
         const { expiresAt } = otpData[otpData.length - 1];
-        console.log(expiresAt, "This is expiry");
         const correctOtp = otpData[otpData.length - 1].otp;
 
         if (otpData && expiresAt < Date.now()) {
             return res.status(401).json({ message: "Otp Expired" });
         }
         if (correctOtp === otp) {
-            console.log("OTP is Correct");
             await Otp.deleteMany({ studentId: instructorId })
             await Instructor.updateOne({ _id: instructorId }, { $set: { is_verified: true } })
-            console.log("Instructor Verified");
             res.status(200).json({ message: "Otp verified Successfully, Please Login to Continue" })
 
         }
@@ -83,14 +76,9 @@ export const instructorEmailOtpVerification = async (req, res) => {
 
 export const resendInstructorOtp = async (req, res) => {
     try {
-        console.log("haaai trying to resend otp");
         const { instructorEmail } = req.body;
-        console.log(instructorEmail);
         const { _id, name, email } = await Instructor.findOne({ email: instructorEmail });
-        console.log("this is id", _id);
-
         const otpId = sendMailOtp(name, email, _id)
-        console.log(otpId);
         if (otpId) {
             res.status(200).json({ message: `Otp Has been resend to ${email} ` })
         }
@@ -109,11 +97,8 @@ export const InstructorLogin = async (req, res) => {
     try {
         const { email, password } = req.body;
         const instructor = await Instructor.findOne({ email: email });
-        console.log(instructor);
-
         if (instructor) {
             const correctPassword = await bcrypt.compare(password, instructor.password)
-            console.log(correctPassword, "password correct");
             if (instructor.is_blocked === false) {
                 if (correctPassword) {
                     const token = jwt.sign(
@@ -126,7 +111,6 @@ export const InstructorLogin = async (req, res) => {
                         process.env.INSTRUCTOR_SECRET,
                         { expiresIn: "1h" }
                     );
-                    console.log(token);
                     res.status(200).json({ instructor, token, message: `welcome ${instructor.name}` })
                 }
                 else {
@@ -147,18 +131,14 @@ export const InstructorLogin = async (req, res) => {
 
 
 export const instructorforgotpassword = async (req, res) => {
-    console.log("Instructor forgot Password Backend");
     const { email } = req.body;
     const instructorData = await Instructor.findOne({ email: email })
-    console.log(instructorData);
     if (instructorData) {
         await sendResetpasswordIns(instructorData.name, instructorData.email, instructorData._id)
-
         res.status(201).json({ instructorData, message: "Please Check Your email" })
 
     }
     else {
-        console.log("Ingane oru Instructor Illa");
         res.status(404).json({ message: "Email Not found please Try Again" })
     }
 
@@ -166,18 +146,11 @@ export const instructorforgotpassword = async (req, res) => {
 }
 
 
-
-
 export const changePassword = async (req, res) => {
     try {
-        console.log("helo trying to change instructor Password");
         const { id, newPassword } = req.body
         const hashedPassword = await securePassword(newPassword)
-        console.log(id, "this is instructor Id");
-        console.log(newPassword, "this NEW Paswword");
-
         const instructorData = await Instructor.findByIdAndUpdate({ _id: id }, { $set: { password: hashedPassword } });
-        console.log("password has been changed in Instructor ");
         res.status(201).json({ message: "Password Changed Successfully" })
     } catch (error) {
         res.status(500).json({ message: "Internal server error" })
@@ -189,15 +162,12 @@ export const changePassword = async (req, res) => {
 
 export const googleSignins = async (req, res) => {
     try {
-        console.log("In Google signin");
         const userData = req.body.user
         const { email, displayName } = userData;
         const instructorData = await Instructor.findOne({ email: email })
         if (instructorData) {
             const token = jwt.sign({ id: instructorData._id }, process.env.INSTRUCTOR_SECRET);
             const { password: pass, ...rest } = instructorData._doc;
-            console.log("Instructor Can Login");
-
             res.status(200)
                 .json({ instructorData, token, message: `Welcome Back ${instructorData.name}` })
         }
@@ -211,11 +181,8 @@ export const googleSignins = async (req, res) => {
             })
             const newInstructor = await instructor.save();
             const instructorData = await Instructor.findOne({ email: email })
-            console.log(instructorData.email, "this is email");
             const changed = await Instructor.findOneAndUpdate({ email: instructorData.email }, { $set: { is_verified: true } })
-            console.log(changed, "NEW INSTRUCTOR DATA");
             const token = jwt.sign({ id: instructor._id }, process.env.INSTRUCTOR_SECRET);
-            console.log("New User Registered with Google Account");
             const { password: pass, ...rest } = instructor._doc;
             res.status(200).json({ instructorData, token, message: `Welcome  ${newInstructor.name}` })
 
@@ -232,10 +199,8 @@ export const myCourses = async (req, res) => {
     try {
 
         const { instructorId } = req.params
-        console.log("in backend", instructorId);
         const courses = await Course.find({ instructorId: instructorId }).populate('category');
         if (courses) {
-            console.log(courses, "this is populated Course");
             res.status(200).json({ courses })
         }
     } catch (error) {
@@ -245,14 +210,10 @@ export const myCourses = async (req, res) => {
 
 }
 
-
 export const fetchCouseData = async (req, res) => {
     try {
-        console.log(req.params, "body ");
         const { courseId } = req.params
-        console.log(courseId);
         const courseData = await Course.findOne({ _id: courseId }).populate('category').populate('modules.module');
-        console.log(courseData);
         res.status(200).json({ message: "course data fetched Successfully", courseData })
 
     } catch (error) {
@@ -263,9 +224,7 @@ export const fetchCouseData = async (req, res) => {
 }
 
 export const changeListStatus = async (req, res) => {
-    console.log("haaai")
     const { courseId } = req.body;
-    console.log(courseId);
     const courseDetail = await Course.findOne({ _id: courseId })
 
     if (courseDetail?.is_Listed === true) {
@@ -283,10 +242,8 @@ export const changeListStatus = async (req, res) => {
 export const checkListStatus = async (req, res) => {
     try {
         const { courseId } = req.params
-        console.log(courseId, "checking List status")
         const courseData = await Course.findOne({ _id: courseId })
         const status = courseData?.is_Listed
-        console.log(status);
         res?.status(200).json({ status })
 
     } catch (error) {
@@ -297,11 +254,7 @@ export const checkListStatus = async (req, res) => {
 
 
 export const EditinstructorProfile = async (req, res) => {
-    console.log(req.body);
     const { instructorId, name, phone } = req.body
-
-    console.log(instructorId, name, phone);
-
     await Instructor.updateOne({ _id: instructorId }, { $set: { name: name, phone: phone } })
     const updatedInstructor = await Instructor.findById({ _id: instructorId })
     res.status(200).json({ message: "success", updatedInstructor, })
@@ -313,7 +266,6 @@ export const instructorDatainChat = async (req, res) => {
     try {
         const { instructorId } = req.params;
         const instructor = await Instructor.findOne({ _id: instructorId });
-        console.log(instructor, "Instructor in chat");
         res.status(200).json({ instructor })
     } catch (error) {
         console.log(error);
